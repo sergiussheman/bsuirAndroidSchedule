729828d6e66c4088e47967df70524eb76e3ce5ba
sergiussheman <sergius.sheman@gmail.com>
2016-01-19 11:59:41 +0300

    added android manager for sqLite databases

---
+++ app/src/main/java/com/example/myapplication/dao/SchoolDayDao.java
@@ -0,0 +1,38 @@
+package com.example.myapplication.dao;
+
+import com.example.myapplication.model.Schedule;
+import com.example.myapplication.model.SchoolDay;
+import com.example.myapplication.model.WeekDayEnum;
+
+import java.util.List;
+
+/**
+ * Created by iChrome on 29.12.2015.
+ */
+public class SchoolDayDao {
+    private DBHelper dbHelper;
+
+    public SchoolDayDao(DBHelper helper){
+        setDbHelper(helper);
+    }
+
+    public void saveSchoolWeekToDataBase(List<SchoolDay> week){
+        for(SchoolDay schoolDay : week){
+            for(Schedule schedule : schoolDay.getSchedules()){
+                WeekDayEnum weekDay = WeekDayEnum.getDayByName(schoolDay.getDayName());
+                if(weekDay != null) {
+                    schedule.setWeekDay((long) weekDay.getOrder());
+                }
+                getDbHelper().addScheduleToDataBase(schedule);
+            }
+        }
+    }
+
+    public DBHelper getDbHelper() {
+        return dbHelper;
+    }
+
+    public void setDbHelper(DBHelper dbHelper) {
+        this.dbHelper = dbHelper;
+    }
+}
